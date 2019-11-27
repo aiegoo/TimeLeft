1c77b21b9749b2d062d60e9b2711fbfaf5c5f2b0
Upmaka Umasai <Upmaka.Umasai@eton-solutions.com>
2019-11-27 14:52:29 +0530

    Updates

---
+++ precache-manifest.21f93cefd241239d7640e273e27d5e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc41538cc215c08eb3889a6a5f1f4957",
+    "url": "/TimeLeft/index.html"
+  },
+  {
+    "revision": "958a496f235e69796af0",
+    "url": "/TimeLeft/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "499090bc19ca334809b0",
+    "url": "/TimeLeft/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "958a496f235e69796af0",
+    "url": "/TimeLeft/static/js/2.39813c60.chunk.js"
+  },
+  {
+    "revision": "499090bc19ca334809b0",
+    "url": "/TimeLeft/static/js/main.425889d7.chunk.js"
+  },
+  {
+    "revision": "bd49c8e223597f1390be",
+    "url": "/TimeLeft/static/js/runtime-main.75baf762.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TimeLeft/static/media/logo.25bf045c.svg"
+  }
+]);
